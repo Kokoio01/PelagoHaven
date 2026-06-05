c01627a7fba1e6920c46ff097de4017202494d26
Lukas <210173666+Kokoio01@users.noreply.github.com>
2026-06-05 07:53:31 +0200

    feat: add sidebar indicator

--- src/layouts/appLayput.tsx
+++ src/layouts/appLayput.tsx
@@ -1,10 +1,16 @@
-import {Outlet} from "react-router";
-import {HomeIcon, SettingsIcon, ShelvingUnitIcon} from "lucide-react";
+import {Link, Outlet} from "react-router";
+import {HomeIcon, ShelvingUnitIcon} from "lucide-react";
+
+const tabs = [
+    { name: "Home", path: "/", icon: <HomeIcon size={28}/> },
+    { name: "Library", path: "/#/library", icon: <ShelvingUnitIcon size={28}/>}
+]
 
 export function AppLayout() {
-    return (
+    const currentTab = tabs.find(tab => tab.path === window.location.pathname);
 
-        <div className="flex flex-col h-screen w-screen bg-foreground">
+    return (
+        <div className="flex flex-col h-screen w-screen bg-chrome text-text">
             <header data-tauri-drag-region className="flex h-10 items-center justify-between px-4">
                 <div className="flex items-center gap-2">
                     <div className="w-14"/>
@@ -13,12 +19,13 @@ export function AppLayout() {
             </header>
             <div className="flex flex-1 flex-row min-w-0">
                 <aside className="h-full p-4 w-16">
-                    <nav className="h-full items-center justify-between flex flex-col gap-4">
-                        <div className="flex flex-col gap-4 items-center">
-                            <HomeIcon size={28}/>
-                            <ShelvingUnitIcon size={28}/>
-                        </div>
-                        <SettingsIcon size={28}/>
+                    <nav className="h-full items-center flex flex-col gap-4">
+                        { tabs.map(tab => (
+                            <Link to={tab.path} key={tab.name} className="relative">
+                                {tab.icon}
+                                { currentTab?.name === tab.name ? <div className="absolute bg-accent w-2 top-0 bottom-1 right-10 rounded-r-4xl" /> : undefined}
+                            </Link>
+                        ))}
                     </nav>
                 </aside>
                 <div className="rounded-tl-2xl bg-background text-amber-50 overflow-y-auto w-full p-4">
